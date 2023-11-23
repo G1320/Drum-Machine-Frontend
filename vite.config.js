d5fc500aa2cfdeda39b8f356caccb6ba72432bf5
G1320 <darnellgreen211@gmail.com>
2023-11-23 14:21:24 +0200

    Deploying to render

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Specify the path to your 'dist' folder here
+    outDir: '../Backend/dist/assets',
   },
 });
