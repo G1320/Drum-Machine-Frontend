a69788c5cf30a03a78d994aae964c250fe5a8ab4
G1320 <darnellgreen211@gmail.com>
2024-02-22 12:18:50 +0200

    Audio context is now started when a sound from the soundsList is previewed

--- src/components/sounds/sound-details.jsx
+++ src/components/sounds/sound-details.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Transport } from 'tone';
 import '../../assets/styles/components/sounds/sound-details.scss';
 import { playAudio } from '../../services/sound-service';
 import { useParams } from 'react-router-dom';
@@ -6,6 +7,7 @@ import { useDispatch } from 'react-redux';
 
 import { faVolumeHigh, faXmark, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { getSilence } from '../../services/sound-service';
 
 import { setError } from '../../slices/errorSlice';
 import {
@@ -18,14 +20,29 @@ import {
 } from '../../slices/soundsSlice';
 
 import { useSounds } from '../../hooks/useSounds';
+
 function SoundDetails({ sound, audioRef, className }) {
   const { kitId } = useParams();
   const dispatch = useDispatch();
+  const tempAudioRef = useRef(null);
+  const silenceAudioRef = useRef(null);
 
   const { refetch: refetchSounds } = useSounds(kitId);
 
-  const handleClick = () => {
+  const handleClick = async () => {
+    if (Transport.state === 'stopped') await playSilence();
+
     playAudio(audioRef);
+  };
+
+  const playSilence = async () => {
+    if (!silenceAudioRef.current) {
+      const silence = await getSilence();
+      silenceAudioRef.current = new Audio(silence.src);
+    }
+
+    tempAudioRef.current = silenceAudioRef.current;
+    tempAudioRef.current.play();
   };
 
   const handleKitAction = async () => {
